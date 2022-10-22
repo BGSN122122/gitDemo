418623f38bf0aafc2a4ce8ec418b69ada5ba582f
xudongxu <xudongxu@qq.com>
2022-10-22 20:51:19 +0800

    xxx-4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello world1!");
         System.out.println("Hello world2!");
         System.out.println("Hello world3!");
+        System.out.println("Hello world-master!");
     }
 }
